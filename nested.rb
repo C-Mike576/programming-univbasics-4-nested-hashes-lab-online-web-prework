ccb04d622f0789352c2b41e5ffb791cceac8d615
Michael Calkins <m.calkins576@gmail.com>
2019-10-17 20:23:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-String.programmer_hash[:dennis_ritchie][:languages]
+programmer_hash.String[:dennis_ritchie][:languages]
   
 end
 
